ac9daf7d8e8f9968709a137afae1ffd4e30bab3a
TechQuery <shiy2008@gmail.com>
2025-02-05 21:16:47 +0800

    [migrate] replace Node.js & TSX with Deno

--- scripts/count-reward.ts
+++ scripts/count-reward.ts
@@ -1,6 +1,8 @@
-import "zx/globals";
+import { $, YAML, argv, fs } from "npm:zx";
 
 $.verbose = true;
+
+const [targetFile] = argv._;
 
 const rawTags =
   await $`git tag --list "reward-*" --format="%(refname:short) %(creatordate:short)"`;
@@ -11,8 +13,8 @@ const lastMonthStr = lastMonth.toISOString().slice(0, 7);
 
 const rewardTags = rawTags.stdout
   .split("\n")
-  .filter(line => line.split(" ")[1] >= lastMonthStr)
-  .map(line => line.split(" ")[0]);
+  .filter((line) => line.split(" ")[1] >= lastMonthStr)
+  .map((line) => line.split(" ")[0]);
 
 let rawYAML = "";
 
@@ -23,7 +25,7 @@ const rewards = YAML.parse(rawYAML) as Reward[];
 
 const groupedRewards = Object.groupBy(rewards, ({ id }) => id);
 
-const summaryArray = Object.entries(groupedRewards).map(([id, rewards]) => {
+const summaryList = Object.entries(groupedRewards).map(([id, rewards]) => {
   const reward = rewards!.reduce((acc, { currency, reward }) => {
     acc[currency] ??= 0;
     acc[currency] += reward;
@@ -33,4 +35,7 @@ const summaryArray = Object.entries(groupedRewards).map(([id, rewards]) => {
   return { id, reward };
 });
 
-console.log(YAML.stringify(summaryArray));
+const summaryText = YAML.stringify(summaryList);
+
+if (targetFile) await fs.outputFile(targetFile, summaryText);
+else console.log(summaryText);
--- scripts/share-reward.ts
+++ scripts/share-reward.ts
@@ -1,9 +1,9 @@
-import { components } from "@octokit/openapi-types";
-import "zx/globals";
+import { components } from "npm:@octokit/openapi-types";
+import { $, YAML, argv, fs } from "npm:zx";
 
 $.verbose = true;
 
-const [currency, reward, PR_URL] = argv._;
+const [currency, reward, PR_URL, targetFile] = argv._;
 
 interface PRMeta {
   author: components["schemas"]["simple-user"];
@@ -18,9 +18,12 @@ const users = [author.login, ...assignees.map(({ login }) => login)];
 
 const averageReward = (parseFloat(reward) / users.length).toFixed(2);
 
-const list: Reward[] = users.map(login => ({
+const list: Reward[] = users.map((login) => ({
   id: `@${login}`,
   currency,
-  reward: parseFloat(averageReward)
+  reward: parseFloat(averageReward),
 }));
-console.log(YAML.stringify(list));
+const listText = YAML.stringify(list);
+
+if (targetFile) await fs.outputFile(targetFile, listText);
+else console.log(listText);
